600b0529eb473bf79ae37dbe8ba16a8f134e9c77
Tex <julestexier@live.fr>
2022-04-26 10:49:22 +0200

    creation heroes component

---
+++ src/app/component/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { Heroes } from 'src/app/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  heroes = Heroes
+}
